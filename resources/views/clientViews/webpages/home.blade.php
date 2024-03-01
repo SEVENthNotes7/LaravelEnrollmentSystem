d04763d4c76779cb1e646bd2494d7fccfaab00d4
CedAlegsao <cedrickcuencaalegsao@gmail.com>
2024-03-01 21:53:14 +0800

    code

--- resources/views/clientViews/webpages/home.blade.php
+++ resources/views/clientViews/webpages/home.blade.php
@@ -1,4 +1,30 @@
 @extends('clientViews.layout.clientlayout')
 @section('content')
-<h1>home</h1>
+<div class="container">
+    <div class="title">
+        <h1>home</h1>
+    </div>
+    <div class="form">
+        <form action="#">
+            <div class="form-title">
+                <h1>enrollment form</h1>
+            </div>
+            <div class="messages">
+                <h3>messages</h3>
+            </div>
+            <div class="form-inputs">
+                <label for="fName">fist name:</label>
+                <input type="text" name="fName"><br>
+                <label for="mName">middle name:</label>
+                <input type="text" name="mName"><br>
+                <label for="lName">last name:</label>
+                <input type="text" name="lName"><br>
+                <label for="degree"></label>
+            </div>
+            <div class="btn-submit">
+                <button>Enroll</button>
+            </div>
+        </form>
+    </div>
+</div>
 @stop
--- resources/views/clientViews/webpages/subject.blade.php
+++ resources/views/clientViews/webpages/subject.blade.php
@@ -1,4 +1,27 @@
 @extends('clientViews.layout.clientlayout')
 @section('content')
-<h1>subjects</h1>
+    <div class="container">
+        <div class="title">
+            <h1>subjects</h1>
+        </div>
+        <div class="content">
+            <div class="first-year">
+                <div class="first-sem">
+                    <table>
+                        <thead>
+                            <tr>
+                                
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+                <div class="second-sem">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
